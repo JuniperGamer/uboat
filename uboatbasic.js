54c7820fe6131312022de183e54891d8e001ede6
JuniperGamer <122415988+JuniperGamer@users.noreply.github.com>
2023-04-23 17:07:01 +1000

    scaffold for letter translation

---
+++ uboatbasic.js
@@ -0,0 +1,10 @@
+let a = 0, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 21, u = 31, v = 41, w = 51, x = 61, y = 71, z = 81
+
+
+console.group('Uboat Board');
+ console.error("placeholder")
+ console.warn("placeholder")
+ console.log("this is uboat basic, this is the scaffold of uboat. No features other than pure letter translation can be found here")
+console.groupEnd('Uboat Board');
+
+
