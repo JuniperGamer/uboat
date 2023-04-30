878fb2baa8626accf420a17e9a5386bf929e61fe
JuniperGamer <122415988+JuniperGamer@users.noreply.github.com>
2023-04-30 11:00:31 +1000

    new commands v1.3

--- uboatcommands.js
+++ uboatcommands.js
@@ -1,15 +1,42 @@
 const uboat = {
     help() {
-      console.log("All commands listed:\n - help // Shows commands enabled\n clear // clears the console.\n github.go // takes you to Uboat's github repository");
+      console.log("All commands listed:\n - help // Shows commands available\n clear // clears the console.\n github.go // takes you to Uboat's github repository.\n import // re-imports uboat incase something doesn't work.");
     },
     clear() {
       console.clear();
     },
     github() {
       window.location.href = "https://github.com/JuniperGamer/uboat";
+    },
+    import() {
+      console.log("If for some reason Uboat stops working or doesn't import, reinjecting will fix it. \n awaiting response...")
+      confirm("Would you like to reinject Uboat?")
+      if (confirm("Would you like to reinject Uboat?") == true){
+        return a = 0, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 21, u = 31, v = 41, w = 51, x = 61, y = 71, z = 81
+      } else {
+        alert("Injection cancelled, have a nice day :)");
+      }
+    },
+    mode_enable() {
+      console.log("Debug mode enabled")
+      if (addEventListener(console.log("Debug mode enabled")) == true){
+        console.log("Debug mode is already enabled")
+      } else {
+      const DEBUG = {
+        uboatplus() {
+          console.log("Uboat+ is no longer available to use. Uboat+ will never continue, also how\ndid you find out about these commands?")
+        },
+        mode_disable() {
+          if (addEventListener(console.log("Debug mode enabled")) == true) {
+            const DEBUG = "null"
+          }
+        }
+      }
+      }
+      
     }
-  }
-  
+    }
+
   function help() {
     uboat.help();
   }
@@ -22,4 +49,18 @@ const uboat = {
     uboat.github();
   }
 
-// use uboat.example() to call a command
+  function inject() {
+    uboat.import();
+  }
+
+  function mode_enable() {
+    DEBUG.mode_enable()
+  }
+
+  function mode_disable() {
+    DEBUG.mode_disable()
+  }
+
+  function uboatplus() {
+    DEBUG.uboatplus()
+  }
